f9e3e59cb347f08b7197796816f1986da8fd0069
André Eriksson <aeriksson@users.noreply.github.com>
2025-10-21 10:10:00 +0200

    Update Polytope CLI to release 0.1.37 (51d8a62e7)

--- Formula/polytope-cli.rb
+++ Formula/polytope-cli.rb
@@ -1,37 +1,37 @@
 class PolytopeCli < Formula
   desc "The Polytope CLI"
   homepage "https://polytope.com"
-  version "0.1.36"
+  version "0.1.37"
 
   @@os = "#{OS.mac? ? 'macos' : 'linux'}"
   @@arch = "#{Hardware::CPU.intel? ? 'amd64' : 'arm64'}"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://polytope.com/releases/polytope-cli-0.1.36-macos-amd64.gz"
+      url "https://polytope.com/releases/polytope-cli-0.1.37-macos-amd64.gz"
       sha256 ""
     elsif Hardware::CPU.arm?
-      url "https://polytope.com/releases/polytope-cli-0.1.36-macos-arm64.gz"
+      url "https://polytope.com/releases/polytope-cli-0.1.37-macos-arm64.gz"
       sha256 ""
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://polytope.com/releases/polytope-cli-0.1.36-linux-amd64.gz"
+      url "https://polytope.com/releases/polytope-cli-0.1.37-linux-amd64.gz"
       sha256 ""
     elsif Hardware::CPU.arm?
-      url "https://polytope.com/releases/polytope-cli-0.1.36-linux-arm64.gz"
+      url "https://polytope.com/releases/polytope-cli-0.1.37-linux-arm64.gz"
       sha256 ""
     end
   end
 
   def install
-    bin.install "polytope-cli-0.1.36-#{@@os}-#{@@arch}" => "polytope"
+    bin.install "polytope-cli-0.1.37-#{@@os}-#{@@arch}" => "polytope"
     bin.install_symlink "polytope" => "pt"
   end
 
   test do
-    assert_includes shell_output("#{bin}/polytope --version 2>&1"), "0.1.36-6e719addd-#{@@os}-#{@@arch}"
+    assert_includes shell_output("#{bin}/polytope --version 2>&1"), "0.1.37-51d8a62e7-#{@@os}-#{@@arch}"
   end
 end
